bcb3dab0cc6c2f9f11f587ca745be848883c6bb8
Görkem Şakalak <59141691+Halfgork@users.noreply.github.com>
2022-08-16 00:59:31 +0300

    Add files via upload

---
+++ oddishOrEvenish.js
@@ -0,0 +1,17 @@
+function oddishOrEvenish(n) {
+  var sum = 0;
+
+  while (n > 0) {
+      sum = sum + (n % 10)
+      n = Math.floor(n / 10)
+  }
+
+  if (sum % 2 == 0) {
+      return "Even"
+  }
+
+  return "Odd"
+}
+console.log(oddishOrEvenish(43))
+console.log(oddishOrEvenish(373))
+console.log(oddishOrEvenish(4433))
